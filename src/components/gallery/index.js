7ed2f96e000275d044d1cbe19df7bb92b2a42788
Grahambone37 <electricgrunt37@gmail.com>
2023-02-07 15:54:45 -0800

    got a map of all the postings

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,10 +1,14 @@
 import Posting from "./posting"
 
 export default function Gallery() {
+    const craigPost = require('../../postings')
+    let postList = craigPost.postings.map((data, i) => {
+        return <Posting data={data} key={i} />
+    })
     return (
         <div>
             <h2>Gallery</h2>
-            <Posting />
+            {postList}
         </div>
     )
 }
--- src/components/gallery/posting.js
+++ src/components/gallery/posting.js
@@ -1,7 +1,11 @@
-export default function Posting() {
+export default function Posting(props) {
+let info = props.data
     return (
         <div>
-            <h3>Posting</h3>
+            <h3>{info.title}</h3>
+            <h3>{info.description}</h3>
+            <h3>{info.price}</h3>
+            <img src={info.imageURL} alt="the item"></img>
         </div>
     )
 }
